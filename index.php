3c4d404eac3ed530a96c73bb6e98ba9f7677bc8b
MateusErkmann <hipergeniusx@gmail.com>
2017-05-05 09:24:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<?php
+
+    require 'logica.php';
+
+?>
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Eah Wai</title>
+</head>
+<body>
+
+    <h2> <?= "Olá $tratamento $nome, $lugar." ?> </h2>
+
+</body>
+</html>
---
+++ logica.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$tratamentos = ['CALA BOCA MAGDA!!!','Cê eh o Bixão Memo Eihn','Cheroso'];
+$qualidades = ['paladino','safado','bandidoo'];
+$lugares = ['de Alagoinhas', 'da Irineulândia','de Chapecó'];
+$nome = "Black Birll";
+
+$tratamento = $tratamentos[rand(0,2)];
+$qualidade = $qualidades[rand(0,2)];
+$lugar = $lugares[rand(0,2)];
+
